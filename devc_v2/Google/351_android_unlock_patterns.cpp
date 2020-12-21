4c9b71a68f8e23190f8a342707aa80ae193aac53
VijayNandakumarkumar <vijay.nandakumar30@gmail.com>
2020-12-21 22:14:03 +0530

    Modified Google 351_android_unlock_patterns.cpp, added new solution.

--- devc_v2/Google/351_android_unlock_patterns.cpp
+++ devc_v2/Google/351_android_unlock_patterns.cpp
@@ -29,6 +29,70 @@ Example:
 Given m = 1, n = 1, return 9.
 */
 
+// Solution - 1
+// The below solution seems to be more understandable and was from Signal Cat in medium, 
+// https://medium.com/@rebeccahezhang/leetcode-351-android-unlock-patterns-d9bae4a8a958
+
+#include<bits/stdc++.h>
+using namespace std;
+#define m 3
+#define n 3
+int DFS( vector<bool> &visited, vector<vector<int> > &skip, int cur, int rem) {
+    
+    if (rem < 0) {
+        return 0;
+    }
+    
+    if (rem == 0) {
+        return 1;
+    }
+    
+    visited[cur] = true;
+    
+    int res = 0;
+    for(int i = 0;i<=9;i++) {
+        if(!visited[i] && (skip[cur][i] == 0 || visited[skip[cur][i]])) {
+            res += DFS(visited, skip, i, rem - 1);
+        }
+    }
+    visited[cur] = false;
+    
+    return res;
+}
+
+
+int main() {
+    vector<vector<int> > skip(10, vector<int> (10, 0));
+    skip[1][3] = skip[3][1] = 2;
+    skip[1][7] = skip[7][1] = 4;
+    skip[7][9] = skip[9][7] = 8;
+    skip[9][3] = skip[3][9] = 6;
+    skip[2][8] = skip[8][2] = skip[4][6] = skip[6][4] = skip[1][9]=skip[9][1] = skip[3][7] = skip[7][3] = 5;
+    
+    vector<bool> visited (10, false);
+    int res = 0;
+    
+    for(int i = m;i<=n;i++) {
+        res += DFS(visited, skip, 1, i - 1) * 4;
+        res += DFS(visited, skip, 2, i - 1) * 4;
+        res += DFS(visited, skip, 5, i - 1);
+    
+    cout<<"The number of valid paths are "<<res;
+    
+    // for(int i = 0; i< (int) skip.size();i++) {
+    //     for (int j=0;j<(int) skip[0].size();j++) {
+    //         cout<<skip[i][j]<<"\t";
+    //     }
+    //     cout<<"\n";
+    // }
+    
+    }
+    
+    return 0;
+}
+
+// Solution - 2 from leetfree.
+
 
 #include<bits/stdc++.h>
 using namespace std;
