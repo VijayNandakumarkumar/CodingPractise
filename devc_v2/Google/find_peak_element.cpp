832129f1e24a50debf9c81900f80d5370afbd703
VijayNandakumarkumar <vijay.nandakumar30@gmail.com>
2020-12-28 01:07:22 +0530

    Added correct solution to find peak element 162.

--- devc_v2/Google/find_peak_element.cpp
+++ devc_v2/Google/find_peak_element.cpp
@@ -1,5 +1,45 @@
+/*
+A peak element is an element that is strictly greater than its neighbors.
+
+Given an integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+
+
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
+
+
+// Optimised bin search solution.
+
+int findPeakElement(vector<int>& nums) {
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < nums[mid + 1]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+    }
+    }
+	return left; 
+}
 
 int findPeakElement(vector<int>& nums) {
     for(int i=0;i<(int)nums.size()-1;i++){
