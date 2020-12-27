e062bcd4a96e6277a81bf5736b405bb40ac9a1ab
VijayNandakumarkumar <vijay.nandakumar30@gmail.com>
2020-12-27 13:13:54 +0530

    Modifief edit_distance and find the celebrity.

--- devc_v2/Google/edit_distance_bottom_up.cpp
+++ devc_v2/Google/edit_distance_bottom_up.cpp
@@ -1,3 +1,54 @@
+/*
+learnt from back to back swe.
+72. Edit Distance
+Hard
+
+4911
+
+66
+
+Add to List
+
+Share
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+ 
+
+Example 1:
+
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+Example 2:
+
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+Explanation: 
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
+ 
+
+Constraints:
+
+0 <= word1.length, word2.length <= 500
+word1 and word2 consist of lowercase English letters.
+
+*/
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 int edit_distance(string s1, int s1_len, string s2, int s2_len, int a[][6]){
--- devc_v2/Google/find_the_celebrity.cpp
+++ devc_v2/Google/find_the_celebrity.cpp
@@ -1,3 +1,18 @@
+/*
+277. find the celebrity.
+Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity. The definition of. Celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
+
+Now you want to find out who the celebrity is or verify that there is not one. The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+
+You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrityin). your function should minimize the number of calls to knows
+
+Note: There will be exactly one celebrity if he/she is in the party. Return the celebrity's label if there is a celebrity in the party. If there is
+
+no celebrity, return -1. any of them.
+
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -40,6 +55,41 @@ int find_celebrity(){
 	}
 	return c;
 }
+
+
+int findCelbrity() {
+    
+    
+   int candidate = 0, i = 1;
+   for (;i<N;i++) {
+       if(knows(candidate, i)) {
+           candidate = i;
+       }
+   }
+   
+   for(int i=0;i<N;i++) {
+       if ((i != candidate) && (knows(candidate, i) || !(knows(i, candidate)))) {
+           return -1;
+       }
+   }
+   return candidate;
+    
+}
+
+int findCelebirty_easy() { 
+    for (int i=0;i<N;i++) {
+        int sum = 0;
+        for (int j=0;j<N;j++) {
+            sum += adj[i][j];
+        }
+        if (sum == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
 
 int main(){
 	cout<<find_celebrity();
