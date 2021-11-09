ab70c997000f540545a1938672d480408340c833
VijayNandakumarkumar <vijay.nandakumar30@gmail.com>
2021-11-09 20:25:31 +0530

    Adding optimised solution for rotting oranges in amazon set.

--- devc_v2/amazon/994_rotting_oranges.cpp
+++ devc_v2/amazon/994_rotting_oranges.cpp
@@ -20,6 +20,68 @@ Output: 4
 
 */
 
+// Solution coded on 8/11/21 using vectors
+
+typedef pair<int, int> ipair;
+    
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<ipair> rI; //RottenIndeces;
+        int freshOranges = 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if (grid[i][j] == 1) {
+                    freshOranges++;
+                } else if(grid[i][j] == 2) {
+                    rI.push_back({i, j});
+                } else {
+                    continue;
+                }
+            }
+        }
+        
+        if (freshOranges == 0 ) {
+            return 0;
+        } else if (rI.size() == 0) {
+            return -1;
+        }
+        
+        vector<ipair> dir {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+        int time = 0;
+        vector<ipair> nRi;
+        while(1) {
+            while(rI.size() != 0) {
+                int r = (rI.back()).first;
+                int c = (rI.back()).second;
+                rI.pop_back();
+                for(int i=0; i<dir.size(); i++) {
+                    if (isSafe(r+dir[i].first, c+dir[i].second, m, n, grid)) {
+                        grid[r+dir[i].first][c+dir[i].second] = 2;
+                        freshOranges--;
+                        nRi.push_back({r+dir[i].first, c+dir[i].second});
+                    }
+                }   
+            }
+            time++;
+            if ((nRi.size() == 0) && freshOranges > 0) {
+                cout<<freshOranges<<" "<<nRi.size();
+                return -1;
+            } else if (freshOranges == 0) {
+                return time;
+            }
+            rI.assign(nRi.begin(), nRi.end());
+            nRi.clear();
+        }
+        return -1;
+    }
+    bool isSafe(int i, int j, int m, int n, vector<vector<int>> &grid) {
+        return (i>=0) && (i<m) && (j>=0) && (j<n) && (grid[i][j] == 1);
+    }
+
+// Old solution using strings    
+
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
