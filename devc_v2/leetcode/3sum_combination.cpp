937be9b528d7f9a725b8a6f918410b2eb036f075
Vijaykumar <vijay@Vijaykumars-MacBook-Pro.local>
2026-03-07 17:37:45 -0800

    3sum_improved_solution

--- devc_v2/leetcode/3sum_combination.cpp
+++ devc_v2/leetcode/3sum_combination.cpp
@@ -55,3 +55,59 @@ int main(){
 	s.threeSum(vect);
 	return 0;
 }
+
+/*
+15. 3Sum
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+*/
+
+// Best Solution O(log m + n)
+// Neet code: https://www.youtube.com/watch?v=jzZsG8n2R9A
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        if (nums.size()==0)
+             return {};
+        sort(nums.begin(), nums.end());
+        if (nums[0]>0)
+             return {};
+
+        vector<vector<int>> result;
+        set<vector<int>> sum_set;
+        for (int s=0; s<nums.size(); s++) {
+            if (s>0 && nums[s] == nums[s-1])
+                continue;
+            int a = nums[s];
+            int i = s+1, j = nums.size()-1;
+            while(i<j) {
+                 int sum=a+nums[i] + nums[j];
+                 if (sum>0) {
+                    j--;
+                 } else if (sum<0) {
+                    i++;
+                 } else {
+                    vector<int> vs {a, nums[i], nums[j]};
+                    result.push_back(vs);
+                    i++;
+                    j--;
+                    while(i<j && nums[i] == nums[i-1])
+                        i++;
+                 }
+            }
+            
+        }
+        return result;
+    }
+};
